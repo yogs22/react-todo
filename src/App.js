890ff1d5bfba54825f0c1a2080df075c29c6d4a6
yogs22 <yogi.enjoyers22@gmail.com>
2018-11-27 06:32:26 +0700

    Styling todo

--- src/App.js
+++ src/App.js
@@ -2,18 +2,48 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-          <h1>Todo Application</h1>
-          <form ref="form" onSubmit="{this.AddTodo}">
-              <input type="text" ref="time" placeholder="Time activity" />
-              <input type="text" ref="activity" placeholder="The activity" />
-              <button type="submit" className="btn btn-primary">Save</button>
-          </form>
-      </div>
-    );
-  }
+    constructor() {
+        super();
+        this.state = { todos: [] }
+    }
+
+    addTodo = (e) => {
+        e.preventDefault();
+
+        let time = this.refs.time.value;
+        let activity = this.refs.activity.value;
+
+        this.state.todos.push({ time, activity });
+        this.setState({ todos: this.state.todos });
+
+        this.refs.form.reset();
+        this.refs.time.focus();
+
+        return false;
+    }
+
+    render() {
+        return (
+          <div className="App">
+              <h1>Todo Application</h1>
+                <form ref="form">
+                  <input type="text" ref="time" placeholder="Time activity" />
+                  <input type="text" ref="activity" placeholder="The activity" />
+                  <button onClick={ this.addTodo } className="btn btn-primary">Save</button>
+                </form>
+              <hr />
+              <div>
+                  <ul>
+                      { this.state.todos.map((data, i) =>
+                          <li key={i}>
+                              <button onClick={ ()=> this.removeTodo(i) }>Delete</button> { data.time } : { data.activity }
+                          </li>
+                      ) }
+                  </ul>
+              </div>
+          </div>
+        );
+    }
 }
 
 export default App;
